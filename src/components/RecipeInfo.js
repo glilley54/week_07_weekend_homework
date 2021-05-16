8647cd2533a0904e95570a9cca240b1757cf9de3
glilley54 <glilley54@gmail.com>
2021-05-16 18:58:09 +0100

    added diagrams

--- src/components/RecipeInfo.js
+++ src/components/RecipeInfo.js
@@ -12,11 +12,7 @@ const RecipeInfo = ({selectedRecipe, onButtonClick}) => {
             <h2>{selectedRecipe.title}</h2>
             <img id = "image" src={selectedRecipe.thumbnail}></img>
             <h2>Ingredients:</h2>
-            <p>{selectedRecipe.ingredients}</p>
-
-          
-
-            
+            <p>{selectedRecipe.ingredients}</p>            
             <button class="button" onClick={handleClick}>Add to Favourites</button>
 
         </>
